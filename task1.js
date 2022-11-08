61200e43a87f074e266093b3e258c0926873138b
manelduv <duval.manel@gmail.com>
2022-11-08 13:24:57 +0000

    Task 2 done

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
-const cats = ["tony", "daisy", "socks", "rockie" ];
+const cats = ["tony", "daisy", "socks", "rockie"];
 
 const capitalisedCats = cats.map((cat) => {
-    const capitalisedCats = cat.toUpperCase()
-    console.log(capitalisedCats)
-    return capitalisedCats
-})
+  const capitalisedCats = cat.toUpperCase();
+  console.log(capitalisedCats);
+  return capitalisedCats;
+});
--- task2.js
+++ task2.js
@@ -1,11 +1,16 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+
+let bListAnimals = animals.filter(function (animals) {
+  for (let i = 0; i < animals.length; i++) {
+    return animals[i].charAt(0) === "b";
+  }
+});
+console.log(bListAnimals);
