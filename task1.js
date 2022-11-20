fb52cb881e8d8190197e92ca94244d08611bba6b
Tendai Karuwo <tendaikaruwo96@gmail.com>
2022-11-20 20:34:45 +0000

    task 3 complete

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 const cats = ["tony", "daisy", "socks", "rockie"];
 
 const capitalisedCats = cats.map((cat) => {
-  const capitalisedCats = cat.toUpperCase();
-  console.log(capitalisedCats);
-  return capitalisedCats;
+  const capitalisedCat = cat.toUpperCase();
+  console.log(capitalisedCat);
+  return capitalisedCat;
 });
--- task3.js
+++ task3.js
@@ -1,1 +1,17 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+let n3 = 3;
+let n5 = 5;
+let n60 = 60;
+let n90 = 90;
+
+n3 = someNumbers.some(n => n % 3 === 0);
+n5 = someNumbers.some(n => n % 5 === 0);
+n60 = someNumbers.some(n => n % 60 === 0);
+n90 = someNumbers.some(n => n % 90 === 0);
+
+console.log(n3);
+console.log(n5);
+console.log(n60);
+console.log(n90);
+
