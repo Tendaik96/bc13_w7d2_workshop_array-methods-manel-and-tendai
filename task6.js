8a68f9f960574e626c557d5c502f34b65ba32bb5
Tendai Karuwo <tendaikaruwo96@gmail.com>
2022-11-21 18:35:59 +0000

    task complete

--- task6.js
+++ task6.js
@@ -1,5 +1,12 @@
 const broomCupboard = [2, 4, 1, 3, 7, 5, 8, 6, 10, 9, 0];
 
+//6a
+const byOrder = broomCupboard.sort((a, b) => {
+  return a - b;
+});
+console.log(byOrder);
+
+//6b
 const potions = [
   { name: "polyjuice potion", shelf: 2 },
   { name: "draught of living death", shelf: 4 },
@@ -7,6 +14,12 @@ const potions = [
   { name: "veritaserum", shelf: 3 },
 ];
 
+const byShelfOrder = potions.sort((a, b) => {
+  return b.shelf - a.shelf;
+});
+console.log(byShelfOrder);
+
+//6c
 const students = [
   "Cedric Diggory",
   "Padma Patil",
@@ -16,3 +29,23 @@ const students = [
   "Luna Lovegood",
   "Gregory Goyle",
 ];
+
+const compareStrings = (a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+
+  return 0;
+};
+
+const compare = (a, b) => {
+  const splitA = a.split(" ");
+  const splitB = b.split(" ");
+  const lastA = splitA[splitA.length - 1];
+  const lastB = splitB[splitB.length - 1];
+
+  return lastA === lastB
+    ? compareStrings(splitA[0], splitB[0])
+    : compareStrings(lastA, lastB);
+};
+students.sort(compare);
+console.log(students);
